a57c3aa22a94beacfa3ce4dc9cb4b97333324b5f
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-10-21 03:35:52 -0700

    expunge win32

--- platforms/win32/vm/codeZoneControlARM64.h
+++
@@ -1,4 +0,0 @@
-// defines for controlling the memory permissions of the Cogit's code zone on arm64
-// Windows allows the use of PAGE_EXECUTE_READWRITE so nothing need be done and
-// we can defer to the default macro implementations in cogitARMv8.c
-#include <processthreadsapi.h> // For FlushInstructionCache
